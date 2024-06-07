4d923ffda92a7256fe53d830cc48e35eabfe94cc
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-06-07 12:02:32 -0500

    island work; light work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+import { Island } from '../models/Island'
 
      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP 
@@ -14,7 +15,13 @@ const Home = () => {
       camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          
+        <directionalLight />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
+
+        <Island />  
         </Suspense>
       </Canvas>
       </section>
