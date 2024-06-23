1732a98217a38a4786feb3ac21aded43908b6084
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-06-23 16:05:07 -0500

    ++ event hanlders; useeffect

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -28,18 +28,65 @@ const handlePointerDown = (e) => {
   e.stopProgation();
   e.preventDefault();
   setIsRotating(true);
+
+  const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+  lastX.current = clientX;
 }
 
 const handlePointerUp = (e) => {
   e.stopProgation();
   e.preventDefault();
   setIsRotating(false);
+
+  const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+  const delta = (clientX - lastX.current) / viewport.width;
+
+  islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+  lastX.current = clientX;
+  rotationSpeed.current = delta * 0.01 * Math.PI;
 }
 
 const handlePointerMove = (e) => {
   e.stopProgation();
   e.preventDefault();
+
+  if(isRotating) handlePointerUp(e);
 }
+
+const handleKeyDown = (e) => {
+  if(e.key === 'ArrowLeft') {
+    if(!isRotating) setIsRotating(true);
+    islandRef.current.rotation.y += 0.01 * Math.PI;
+  } else if(e.key === 'ArrowRight') {
+    if(!isRotating) setIsRotating(true);
+    islandRef.current.rotation.y -= 0.01 * Math.PI;
+  }
+}
+
+const handleKeyUp = (e) => {
+  if(e.key === 'ArrowLeft' || e.key === 'ArrowRight')  {
+    setIsRotating(false);
+  }
+}
+
+
+useEffect(() => {
+  document.addEventListener('pointerdown', handlePointerDown);
+  document.addEventListener('pointerup', handlePointerUp);
+  document.addEventListener('pointermove', handlePointerMove);
+  document.addEventListener('keydown', handleKeyDown);
+  document.addEventListener('keyup', handleKeyUp);
+
+  return () => {
+    document.removeEventListener('pointerdown', handlePointerDown);
+    document.removeEventListener('pointerup', handlePointerUp);
+    document.removeEventListener('pointermove', handlePointerMove);
+    document.removeEventListener('keydown', handleKeyDown);
+    document.removeEventListener('keyp', handleKeyUp);
+  }
+}, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
 
   return (
     <a.group ref={islandRef} {...props}>
